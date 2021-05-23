c5d86dbdd6dd82b2688a8c3b8d4271fc844e6261
Janith Harinda <jharinda@gmail.com>
2021-05-23 10:45:08 +0530

    Channnel iteration created

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -41,13 +41,10 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
+    for (int channel = 0; channel < bufferToFill.buffer->getNumChannels();channel++)
+    {
 
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    }
 }
 
 void MainComponent::releaseResources()
